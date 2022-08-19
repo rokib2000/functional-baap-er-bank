dccc2fb88612708c6b0c768184ea51fe5943a8b2
rokib2000 <rokib0w1@gmail.com>
2022-08-20 00:17:18 +0600

    withdraw functionalaty add

--- js/withdraw.js
+++ js/withdraw.js
@@ -1,1 +1,14 @@
-console.log("w");
+document.getElementById("withdraw-submit").addEventListener("click", function () {
+  const newWithdrawAmount = getValueFormInputField("withdraw-field");
+  const previousWithdraw = getValueFormTextField("withdraw-total");
+
+  const newWithdrawTotal = previousWithdraw + newWithdrawAmount;
+
+  setValueTextElement("withdraw-total", newWithdrawTotal);
+
+  const previousBalance = getValueFormTextField("balance-total");
+
+  const newBalanceTotal = newWithdrawAmount + previousBalance;
+
+  setValueTextElement("balance-total", newBalanceTotal);
+});
